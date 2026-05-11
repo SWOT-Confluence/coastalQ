8184ef7d503dc6262ff8911de2d702741dee038b
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2026-05-10 21:19:10 -0700

    changed coastalq casing to avoid issues

--- terraform/modules/coastalQ/confluence-busboi.tf
+++ terraform/modules/coastalQ/confluence-busboi.tf
@@ -1,15 +1,15 @@
 # Job Definition
 resource "aws_batch_job_definition" "generate_batch_jd_coastalQ" {
-  name = "${var.prefix}-coastalQ"
+  name = "${var.prefix}-coastalq"
   type = "container"
   platform_capabilities = ["FARGATE"]
   propagate_tags = true
   tags = {
-    job_definition = "${var.prefix}-coastalQ"
+    job_definition = "${var.prefix}-coastalq"
   }
 
   container_properties = jsonencode({
-    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-coastalQ:${var.image_tag}"
+    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-coastalq:${var.image_tag}"
     executionRoleArn = var.iam_execution_role_arn
     jobRoleArn = var.iam_job_role_arn
     fargatePlatformConfiguration = {
@@ -66,5 +66,5 @@ resource "aws_batch_job_definition" "generate_batch_jd_coastalQ" {
 
 # Log group
 resource "aws_cloudwatch_log_group" "cw_log_group" {
-  name = "/aws/batch/job/${var.prefix}-coastalQ/"
+  name = "/aws/batch/job/${var.prefix}-coastalq/"
 }
